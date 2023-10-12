81ed54084c26da905b7cac13342ae9201aa01f33
Aldymanopo <147712177+Aldymanopo@users.noreply.github.com>
2023-10-12 19:25:04 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
-const weatherAPI =
-    "http://api.weatherapi.com/v1/current.json?key=e090ce90580d4968b64103830231210&q=Manado&aqi=no";
+const weatherAPI = "http://api.weatherapi.com/v1/current.json?key=e090ce90580d4968b64103830231210&aqi=no";
 const keyword = document.querySelector(".keyword");
 const btnSearch = document.querySelector(".btn-search");
-
 const container = document.getElementById("container");
 
 btnSearch.onclick = () => {
-    fetch(`${weatherAPI}&q=${keyword.value}`)
-        .then((res) => res.json())
+    const location = keyword.value.trim();
+    if (!location) {
+        alert("Masukkan lokasi yang valid");
+        return;
+    }
+
+    const apiURL = `http://api.weatherapi.com/v1/current.json?key=e090ce90580d4968b64103830231210&q=${location}&aqi=no`;
+
+    fetch(apiURL)
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error('Gagal mengambil data cuaca');
+            }
+            return res.json();
+        })
         .then((data) => {
             console.log(data);
-            let element = "";
-
-            element = showElement(data);
-
+            let element = showElement(data);
             container.innerHTML = element;
+        })
+        .catch((error) => {
+            console.error('Ada kesalahan:', error);
+            alert('Terjadi kesalahan saat mengambil data cuaca.');
         });
 };
 
